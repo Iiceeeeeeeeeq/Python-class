696cdade1476cba46ff642354fd81f85bab9fe90
Iiceeeeeeeeeq <iiceeq.std@gmail.com>
2025-06-05 02:24:47 +0700

    4/06/68

---
+++ #removeprefix(prefix).py
@@ -0,0 +1,9 @@
+#removeprefix(prefix) - ตัดคำนำหน้าสตริง
+#removesuffix(suffix) - ตัดคำลงท้ายสตริง
+
+a= "HelloEmma"
+result = a.removeprefix("Hello")
+print(result)
+
+result = a.removesuffix("a")
+print(result)
---
+++ print.py
@@ -0,0 +1,3 @@
+print ("i love" , "python" , 1234 , sep="|")
+print ("i love" , "python" , 1234 , sep="|", end="|")
+
