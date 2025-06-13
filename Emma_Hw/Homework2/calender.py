ebb63389748143dbf4397946ba8d3f60246cd0b2
PearMaKk <pearoof555@gmail.com>
2025-06-13 13:14:59 +0700

    rr

--- Emma_Hw/Homework2/calender.py
+++ Emma_Hw/Homework2/calender.py
@@ -3,19 +3,8 @@ D = input("Day: ")
 M = int(input("Mouth: "))
 Y = input("Year: ")
 
-M_dic = {
-    1 : "January",
-    2 : "February",
-    3 : "March",
-    4 : "April",
-    5 : "May",
-    6 : "June",
-    7 : "July",
-    8 : "August",
-    9 : "September",
-    10 : "October",
-    11 : "November",
-    12 : "December"
-}
+MONTH = [
+    "","January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"
+]
 
-print(f"{M_dic[M]}  {D} , {Y}")
+print(f"{MONTH[M]}  {D} , {Y}")
