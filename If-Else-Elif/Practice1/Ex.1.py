2a61067d902b422094613bab35df801f45bc9017
PearMaKk <pearoof555@gmail.com>
2025-06-19 01:43:06 +0700

    .

---
+++ If-Else-Elif/Practice1/Ex.1.py
@@ -0,0 +1,6 @@
+num = input("กรุณาใส่ตัวเลขมาสามตัว และโปรดเว้นช่องว่างแต่ละตัวเลข : ").split()
+number = [int(num[0]) , int(num[1]) , int(num[2])]
+number_new = sorted(number)
+
+#หาค่ามัยฐาน
+print (number_new[1])
