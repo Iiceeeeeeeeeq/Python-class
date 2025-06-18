3cf9b99e208a2aea60ba1ebb6a18ba2ea0542aab
PearMaKk <pearoof555@gmail.com>
2025-06-18 20:17:31 +0700

    f

--- Emma_Hw/EmmaTEST/Untitled-1.py
+++ Emma_Hw/EmmaTEST/Untitled-1.py
@@ -2,5 +2,9 @@
 #     for i in range(times):
 #         print(f"{i+1}: {name}")
 # repeat_name("Kai", 3) 
-a = "str"
-type(a)
+# a = "str"
+# type(a)
+for i in range(5):
+    print(i)
+    if i == 3:
+        break
