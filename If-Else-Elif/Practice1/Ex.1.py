66782659524408b7b3783ab636ba7e212b417047
Iiceeeeeeeeeq <iiceeq.std@gmail.com>
2025-06-21 01:29:53 +0700

    .

--- If-Else-Elif/Practice1/Ex.1.py
+++ If-Else-Elif/Practice1/Ex.1.py
@@ -1,10 +1,6 @@
 num = input("กรุณาใส่ตัวเลขมาสามตัว และโปรดเว้นช่องว่างแต่ละตัวเลข : ").split()
 number = [int(num[0]) , int(num[1]) , int(num[2])]
 number_new = sorted(number)
+#หาค่ามัธยฐาน
+print (number_new[1])
 
-#หาค่ามัยฐาน
-<<<<<<< HEAD
-print (number_new[1])
-=======
-print (number_new[1])
->>>>>>> 15661a26b27b32f87a1a822ad5977d622c6b2b0a
--- If-Else-Elif/Practice1/Ex.6.py
+++ If-Else-Elif/Practice1/Ex.6.py
@@ -4,8 +4,4 @@ x = round(a**(1/3))
 if x**3 == a:
     print(x)
 else:
-<<<<<<< HEAD
     print("not found")
-=======
-    print("not found")
->>>>>>> 15661a26b27b32f87a1a822ad5977d622c6b2b0a
