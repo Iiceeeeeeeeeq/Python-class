15661a26b27b32f87a1a822ad5977d622c6b2b0a
PearMaKk <pearoof555@gmail.com>
2025-06-20 20:44:08 +0700

    .

--- Emma_Hw/EmmaTEST/Untitled-1.py
+++ Emma_Hw/EmmaTEST/Untitled-1.py
@@ -1,10 +1,11 @@
-# def repeat_name(name, times):
-#     for i in range(times):
-#         print(f"{i+1}: {name}")
-# repeat_name("Kai", 3) 
-# a = "str"
-# type(a)
-for i in range(5):
-    print(i)
-    if i == 3:
-        break
+print("guess a correct number 1-10")
+number = 0 #ต้องประกาศไว้ก่อนเพื่อให้เงื่อนไขของลูปทำงานได้
+
+while number != 8:
+    number = int(input("guess the number : "))
+    if number > 8:
+        print("incorrect, try lower number")
+    elif number < 8:
+        print("incorrect, try higher number")
+    else:
+        print("CORRECT!")
